c519f017485ac738760b07e1ab38e405fbf1fa18
Hung <hung.vq98@gmail.com>
2021-04-23 13:18:39 +0700

    Login & Register Best UI

--- TechEquip/TechEquip/Areas/User/Controllers/UserSiteController.cs
+++ TechEquip/TechEquip/Areas/User/Controllers/UserSiteController.cs
@@ -13,5 +13,10 @@ namespace TechEquip.Areas.User.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
     }
 }
